3a88c9457f1ec5077336e6d3494309154678568d
Ay Has <ahashim983@yahoo.com>
2019-04-01 21:25:33 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,20 @@
 # Code your solution here!
+
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  inp = gets.chomp
+  if inp == "exit"
+    puts "Goodbye!"
+  else
+    comp = rand(1..6)
+    if inp.to_i == comp
+      puts "You guessed the correct number!"
+      run_guessing_game
+    elsif inp.to_i != comp
+      puts "The computer guessed #{comp}."
+      run_guessing_game
+    end
+  end
+end
+
+#puts run_guessing_game
