0fd00e7d42d0718aa2f10c8e41dad6ea25e17dc8
Ay Has <ahashim983@yahoo.com>
2019-04-01 21:14:17 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,12 +4,17 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   inp = gets.chomp
   until inp == "exit" do
+    if inp == "exit"
+      break
+    end
     comp = rand(1..6)
     if inp == comp
       puts "You guessed the correct number!"
     else
       puts "The comp guessed #{comp}"
     end
+    inp = gets.chomp
+    
   end
 end
     
